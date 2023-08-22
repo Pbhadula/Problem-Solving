d71d50d943642fe0965888fa9c0694aa275a04f4
Prashant Bhadula <30542866+Pbhadula@users.noreply.github.com>
2023-08-22 20:21:50 +0530

    Create SpanOfArray.java

---
+++ src/com/dsa/arrays/SpanOfArray.java
@@ -0,0 +1,34 @@
+package com.dsa.arrays;
+
+import java.util.Scanner;
+
+public class SpanOfArray {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Enter no of elements in array :");
+        int n = s.nextInt();
+        System.out.println("Enter array element :");
+        int[] arr = new int[n];
+
+        for(int i=0; i< arr.length; i++) {
+            arr[i] = s.nextInt();
+        }
+
+        int max = arr[0];
+        int min = arr[0];
+
+        for(int i=1; i< arr.length; i++) {
+            if(arr[i]>max) {
+                max = arr[i];
+            }
+
+            if(arr[i]<min) {
+                min = arr[i];
+            }
+        }
+
+        int span = max - min;
+        System.out.println(span);
+    }
+}
